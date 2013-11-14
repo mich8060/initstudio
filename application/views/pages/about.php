7265a1abe1638094cc4b68b75a127aac228ad946
Michael Stevens <mich8060@hotmail.com>
2013-11-14 11:39:02 -0700

    Changes to profile page
    Change the intro section of the profile page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,35 +1,39 @@
-<section class="about">
+<section class="profile">
 
 	<div class="container">
 		<div class="box-grid">
-			<div class="grid_7">
-				<h3 class="lead">Hello, I'm Michael Stevens</h3>
-				<h6 class="lead">UX Designer <span>&amp;</span> UI Developer</h6>
+			<div class="profile-intro">
+				<div class="grid_7">
+					<h1 class="lead">Hello, I'm Michael Stevens</h1>
+					<h5 class="lead">Sr. UX Designer at <a href="http://www.backcountry.com">Backcountry.com</a></h5>
+					<h5 class="lead">North Salt Lake, Utah</h5>
+					<p>
+						Connect
+						<a href="#">Message</a>
+						Recommend
+					</p>
+					<p><strong>Skills &amp; Expertise</strong></p>
+					<span class="ui-pill">User Experience</span>
+					<span class="ui-pill">User-centered Design</span>
+					<span class="ui-pill">Interaction Design</span>
+					<span class="ui-pill">Typography</span>
+					<span class="ui-pill">Usability Testing</span>
+					<span class="ui-pill">HTML</span>
+					<span class="ui-pill">Rapid Prototyping</span>
+					<span class="ui-pill">Web Design</span>
+					<span class="ui-pill">User Interface Design</span>
+				</div>
 			</div>
 			<div class="clear"></div>
-			<div class="grid_6">
+			<div class="grid_8">
+				<h5>About me</h5>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend erat sed odio condimentum, aliquam lacinia tortor tincidunt. Sed cursus enim felis, quis euismod eros hendrerit ac. Nulla nec euismod orci. Sed facilisis ultrices ullamcorper. In ornare justo eget pretium mattis. Curabitur tristique in quam at mattis. Donec mattis leo eu orci porttitor, eu volutpat purus dapibus. Phasellus id neque in nisl viverra faucibus. Vestibulum laoreet aliquet mauris elementum dapibus. Ut ante ante, scelerisque eu enim eget, porta faucibus massa. Maecenas nibh lorem, volutpat ac velit non, pretium tincidunt nisi. Aenean eu euismod leo, consequat fringilla purus. Nunc eu sem ut tellus rhoncus auctor at eu sem. Nunc sollicitudin blandit laoreet. Pellentesque et nunc odio.
 				</p>
-			</div>
-			<div class="clear"></div>
-			<div class="grid_4">
-				<p class="paragraph-2">
-					<strong>Design</strong>
-					is the marriage of form &amp; content. "When form predominates, meaning is blunted. When content predominates, interest lags." &ndash; Paul Rand. Design is the fusion of form &amp; Content. 
-				</p>
+				<h5>Work History</h5>
 			</div>
 			<div class="grid_4">
-				<p class="paragraph-2">
-					<strong>Born</strong> in Thornton, Colorado in 1982. I moved to Utah when I was about 10. Since then I have lived just north of Salt Lake City in Davis county. I am happily married with one eight year old son.
-				</p>
-			</div>
-			<div class="clear"></div>
-			<div class="grid_4">
-				<p class="paragraph-2">
-					<strong>Career</strong>
-					I began designing websites as early as high school. It got my first job with a company called <a href="http://www.backcountry.com" target="_blank">Backcountry.com</a> where I started development than later moved into the world of design. 
-				</p>
+				<h5>Recommendations</h5>
 			</div>
 			<div class="clear"></div>
 		</div>
